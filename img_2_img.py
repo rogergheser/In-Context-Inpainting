147d7ad35882febbab99da8e8ba9ebf1d43954f1
matteomaske <matteomascherin@gmail.com>
2025-01-12 09:45:32 +0100

    File to test diffusers default pipeline

--- img_2_img.py
+++ img_2_img.py
@@ -1,14 +1,12 @@
-from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_inpaint import StableDiffusionInpaintPipeline
+from diffusers.pipelines import StableDiffusionImg2ImgPipeline
 from diffusers.utils import load_image, make_image_grid
 
 import torch
 from PIL import Image
 
 # use from_pipe to avoid consuming additional memory when loading a checkpoint
-pipeline = StableDiffusionInpaintPipeline.from_pretrained("stabilityai/stable-diffusion-2-1-base",
-                                                torch_dtype = torch.float16,
-                                                variant = "fp16",
-                                                use_safetensor=True).to("mps")
+pipeline = StableDiffusionImg2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-2-1-base",
+                                                use_safetensor=True).to("cuda")
 
 img_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/sdxl-text2img.png"
 mask_url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/sdxl-inpaint-mask.png"
@@ -16,9 +14,9 @@ mask_url = "https://huggingface.co/datasets/huggingface/documentation-images/res
 # init_image = load_image(img_url)
 # mask_image = load_image(mask_url)
 
-init_image = Image.open("inputs/img.png")
-mask_image = Image.open("inputs/mask.png")
+init_image = Image.open("blended-latent-diffusion/inputs/img.png")
+mask_image = Image.open("blended-latent-diffusion/inputs/mask.png")
 
-prompt = "A dog"
-image = pipeline(prompt=prompt, image=init_image, mask_image=mask_image, strength=0.85, guidance_scale=12.5).images[0]
-make_image_grid([init_image, mask_image, image], rows=1, cols=3).save("outputs/img.png")
+prompt = "In a world where the sky is green and the grass is blue."
+images = pipeline(prompt=prompt, image=init_image, strength=0.85, guidance_scale=12.5, num_images_per_prompt=3).images
+make_image_grid(images, rows=1, cols=3).save("outputs/img.png")
