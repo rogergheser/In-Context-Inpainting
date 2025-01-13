a44e297d649f99c35cb13102be9792febdca378e
rogergheser <w100roger@gmail.com>
2025-01-13 11:35:19 +0100

    Integrated Image2TextEmbedder

---
+++ Image2TextEmbedder.py
@@ -0,0 +1,49 @@
+import torch
+
+from PIL import Image
+from transformers import CLIPProcessor, CLIPModel
+
+
+
+class Image2TextEmbedder():
+    def __init__(self,
+                 clip_path: str = "openai/clip-vit-large-patch14",
+                 device: str = "cuda",
+                 alpha: float = 0.6,
+                 onlyprompt: bool = False,
+                 edit: bool = True):
+        self.clip_model = CLIPModel.from_pretrained(clip_path).to(device)
+        self.processor = CLIPProcessor.from_pretrained(clip_path)
+        self.inv_text = torch.linalg.pinv(self.clip_model.text_projection.weight, atol=0.3)
+        self.visual_projection = self.clip_model.visual_projection.weight
+        self.alpha = alpha
+        self.onlyprompt = onlyprompt
+        self.edit = edit
+        self.device = device
+
+    def __call__(self, image, text_embeddings, text_masks):
+        image = Image.open(image).convert("RGB")
+        clip_image = self.processor(None, image, return_tensors='pt').pixel_values.to(self.device)
+        image_emb = self.clip_model.vision_model(pixel_values=clip_image)
+        image_emb = image_emb.pooler_output
+
+        image_emb_proj = image_emb @ self.visual_projection.T
+        image_emb_proj = image_emb_proj @ self.inv_text.T
+        image_emb_proj = image_emb_proj / image_emb_proj.norm(dim=1, keepdim=True)
+        image_emb_proj = 27.5 * image_emb_proj # Empirically determined text embedding norm
+
+        convert_text_embeddings = torch.zeros_like(text_embeddings)
+        convert_text_embeddings[:, 0] = text_embeddings[:, 0]
+        convert_text_embeddings[:, 1:] = image_emb_proj.unsqueeze(1)
+
+        convert_edit_embeddings  = text_embeddings.clone()
+        convert_edit_embeddings[:, text_masks.sum(1)[0]-1:] = image_emb_proj.unsqueeze(1) + self.alpha * text_embeddings[:, text_masks.sum(1)[0]-1:]
+
+        if self.onlyprompt:
+            prompt_embeds = text_embeddings
+        elif self.edit:
+            prompt_embeds = convert_edit_embeddings
+        else:
+            prompt_embeds = convert_text_embeddings
+
+        return prompt_embeds.squeeze(0)
--- image_blending.py
+++ image_blending.py
@@ -4,16 +4,11 @@ import numpy as np
 import sys
 sys.path.append('/Users/amirgheser/In-Context-Matting/icm')
 
-from transformers import CLIPProcessor, CLIPModel
+from Image2TextEmbedder import Image2TextEmbedder
 from diffusers import DDIMScheduler, StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
 # from icm.models.feature_extractor.dift_sd import FeatureExtractor
 from PIL import Image
 from tqdm import tqdm
-
-# class DiffusionFeatureExtractor(FeatureExtractor):
-#     def __init__(self):
-#         super().__init__()
-        
     
 class BlendedLatentDiffusion:
     def __init__(self):
@@ -60,7 +55,7 @@ class BlendedLatentDiffusion:
         parser.add_argument(
             "--output_path",
             type=str,
-            default="outputs/res.jpg",
+            default="outputs/",
             help="The destination output path",
         )
         parser.add_argument(
@@ -117,7 +112,7 @@ class BlendedLatentDiffusion:
         height=512,
         width=512,
         num_inference_steps=100,
-        guidance_scale=7.5,
+        guidance_scale=12.5,
         strength=0.5,
         generator=torch.manual_seed(42),
         blending_percentage=0.25
@@ -270,49 +265,6 @@ class BlendedLatentDiffusion:
 
         return mask, org_mask
 
-class Image2TextEmbedder():
-    def __init__(self,
-                 clip_path: str = "openai/clip-vit-large-patch14",
-                 device: str = "cuda",
-                 alpha: float = 0.6,
-                 onlyprompt: bool = False,
-                 edit: bool = False):
-        self.clip_model = CLIPModel.from_pretrained(clip_path).to(device)
-        self.processor = CLIPProcessor.from_pretrained(clip_path)
-        self.inv_text = torch.linalg.pinv(self.clip_model.text_projection.weight, atol=0.3)
-        self.visual_projection = self.clip_model.visual_projection.weight
-        self.alpha = alpha
-        self.onlyprompt = onlyprompt
-        self.edit = edit
-        self.device = device
-
-    def __call__(self, image, text_embeddings, text_masks):
-        image = Image.open(image).convert("RGB")
-        clip_image = self.processor(None, image, return_tensors='pt').pixel_values.to(self.device)
-        image_emb = self.clip_model.vision_model(pixel_values=clip_image)
-        image_emb = image_emb.pooler_output
-
-        image_emb_proj = image_emb @ self.visual_projection.T
-        image_emb_proj = image_emb_proj @ self.inv_text.T
-        image_emb_proj = image_emb_proj / image_emb_proj.norm(dim=1, keepdim=True)
-        image_emb_proj = 27.5 * image_emb_proj # Empirically determined text embedding norm
-
-        convert_text_embeddings = torch.zeros_like(text_embeddings)
-        convert_text_embeddings[:, 0] = text_embeddings[:, 0]
-        convert_text_embeddings[:, 1:] = image_emb_proj.unsqueeze(1)
-
-        convert_edit_embeddings  = text_embeddings.clone()
-        convert_edit_embeddings[:, text_masks.sum(1)[0]-1:] = image_emb_proj.unsqueeze(1) + self.alpha * text_embeddings[:, text_masks.sum(1)[0]-1:]
-
-        if self.onlyprompt:
-            prompt_embeds = text_embeddings
-        elif self.edit:
-            prompt_embeds = convert_edit_embeddings
-        else:
-            prompt_embeds = convert_text_embeddings
-
-        return prompt_embeds.squeeze(0)
-
 if __name__ == "__main__":
     bld = BlendedLatentDiffusion()
     results = bld.edit_image(
@@ -325,4 +277,7 @@ if __name__ == "__main__":
         strength=bld.args.strength,
     )
     results_flat = np.concatenate(results, axis=1)
-    Image.fromarray(results_flat).save(bld.args.output_path)
+    Image.fromarray(results_flat).save(bld.args.output_path + "res_"
+                                       + "alpha_" + str(bld.args.alpha)
+                                       + "strength_" + str(bld.args.strength) 
+                                       + ".jpg")
